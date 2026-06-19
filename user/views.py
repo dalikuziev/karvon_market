17f64c3b11126234af501bbb65951b173b79a722
Diyorbek <dalikuziev@gmail.com>
2026-06-19 22:32:13 +0500

    user faqat o'zini ko'raolsin!

--- user/views.py
+++ user/views.py
@@ -1,16 +1,19 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .models import User
 from .serializers import UserSerializer
 from market.serializers import ProductSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
     def get_queryset(self):
-        return User.objects.all()
-    @action(detail=True, methods=['get'])
+        return User.objects.filter(id=self.request.user.id)
+    @action(detail=False, methods=['get'])
     def products(self, request, *args, **kwargs):
-        user = self.get_object()
-        serializer = ProductSerializer(user.products.all(), many=True)
+        user = request.user
+        products = user.products.all()
+        serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
